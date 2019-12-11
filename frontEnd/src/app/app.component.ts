a640067d89bfab47f85830faa7f5df1fb04900db
Nezzywezzie <montaniz@student.fdu.edu>
2019-12-11 14:59:40 -0500

    drop down links

--- frontEnd/src/app/app.component.ts
+++ frontEnd/src/app/app.component.ts
@@ -5,6 +5,17 @@ import { Component, AfterViewInit, ElementRef } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'UI';
+
+export class AppComponent implements AfterViewInit{
+  constructor(private elementRef: ElementRef){ }
+
+ public getColor(color: string): string {
+    return color;
+  }
+
+
+ ngAfterViewInit () {
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = this.getColor("black");
+ }
+ title = 'UI';
 }
