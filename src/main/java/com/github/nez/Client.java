0fb275759b63ca2b58bb01e3590350b5159760f0
montanizstills <montaniz@student.fdu.edu>
2020-02-09 23:12:13 -0500

    bare bone server

--- src/main/java/com/github/nez/Client.java
+++ src/main/java/com/github/nez/Client.java
@@ -1,58 +1,57 @@
-//package com.github.nez;
-//
-//import java.io.BufferedReader;
-//import java.io.InputStreamReader;
-//import java.io.PrintWriter;
-//import java.net.Socket;
-//import java.util.Scanner;
-//
-//public class Client implements Runnable {
-//    Scanner clientWriter = new Scanner(System.in);
-//    PrintWriter out;
-//    BufferedReader in;
-//    Socket socket;
-//    int portNumber = 8081;
-//
-//    public Client(){
-//        establishConnection();
-//    }
-//
-//    public void establishConnection(){
-//        try{
-//            this.socket = new Socket("localhost",portNumber);
-//            System.out.println(this.socket);
-//
-//        }
-//        catch(Exception e ){
-//            System.err.println("Fatal Connection error!");
-//            throw new Error(e);
-//        }
-//    }
-//
-//    @Override
-//    public void run(){
-//        speak();
-//    }
-//
-//    private void speak() {
-//        try{
+package com.github.nez;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client implements Runnable {
+    Scanner clientWriter = new Scanner(System.in);
+    PrintWriter out;
+    BufferedReader in;
+    Socket socket;
+    int portNumber = 4444;
+
+    public Client(){
+        establishConnection();
+    }
+
+    public void establishConnection(){
+        try{
+            this.socket = new Socket("localhost",portNumber);
+        }
+        catch(Exception e ){
+            System.err.println("Fatal Connection error!");
+            throw new Error(e);
+        }
+    }
+
+    @Override
+    public void run(){
+        speak();
+    }
+
+    private void speak() {
+        try{
 //            this.out = new PrintWriter(socket.getOutputStream(),true);
 //            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//            while(!socket.isClosed()){
-//                String input = this.clientWriter.nextLine();
+            while(!socket.isClosed()){
+                String input = new Scanner(System.in).nextLine();
 //                System.out.println("you say: "+input);
-//                out.write(input);
-//                out.flush();
-//            }
-//        }
-//        catch(Exception e ){
-//            System.out.println("Error running client...");
-//        }
-//    }
-//
-//    public static void main(String[] args) {
-//        Client client = new Client();
-//        client.run();
-//    }
-//
-//}//End Client class
+                out.write(input);
+                out.flush();
+            }
+        }
+        catch(Exception e ){
+            System.out.println("Error running client...");
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+
+}//End Client class
--- src/main/java/com/github/nez/Server.java
+++ src/main/java/com/github/nez/Server.java
@@ -1,94 +1,46 @@
-//package com.github.nez;
-//
-//import java.io.BufferedReader;
-//import java.io.InputStreamReader;
-//import java.io.PrintWriter;
-//import java.net.ServerSocket;
-//import java.net.Socket;
-//import java.util.ArrayList;
-//
-//public class Server implements Runnable {
-//
-//    private ServerSocket serverSocket = null;
-//    private PrintWriter out;
-//    private BufferedReader in;
-//    private Socket socket;
-//    boolean listening = true;
-//    private ArrayList<Client> connectedClients;
-//
-//    public Server() {
-//    }
-//
-//    public void run() {
-//        try {
-//            this.serverSocket = new ServerSocket(8081);
-//            while (listening) {
-//                this.socket = serverSocket.accept();
-////                in = socket.getInputStream()
-//            }
-//        } catch (Exception e) {
-//            throw new Error("could not listen on port: " + serverSocket.getLocalPort());
-//        }
-//    }
-//
-//    private void acceptClients() {
-//        try {
-//
-//        }
-//        catch (Exception e) {
-//            System.out.println("Accept failed on: " + this.serverSocket.getLocalPort());
-//        }
-//    }
-//
-//    public void speak(){
-//        try{
-//            out = new PrintWriter(socket.getOutputStream(), true);
-//            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//            String response=in.readLine();
-//            if(!"".equals(response)){
-//                System.out.println(response);
-//            }
-//        }
-//        catch(Exception e){
-//            throw new Error(e);
-//        }
-//    }
-////    public String decipherMessage(String message) {
-////        String response = "";
-////        String[] messageArray = message.split(" ");
-////        for (String word : messageArray) {
-////            if (word.matches("\\$[a-zA-Z]+\\.[a-zA-Z]+")) {
-////                String[] requestArray = word.split("[$|.]");
-////                String companyName = requestArray[1];
-////                String serviceName = requestArray[2];
-////
-////                Quote quote = new Quote();
-////                quote.populateJsonObject(companyName, "quote");
-////
-////                try {
-////                    ObjectMapper objectMapper = new ObjectMapper();
-////                    quote = (Quote) objectMapper.readValue(quote.getJsonObject().toString(), quote.getObjectClass());
-////                } catch (Exception e) {
-////                    System.out.println("Something is off in the ObjectMapping.");
-////                }
-////
-////                try {
-////                    Method method = quote.getClass().getMethod(serviceName);
-////                    response += " " + method.invoke(quote, null).toString();
-////                } catch (Exception e) {
-////                    System.out.println("Something is off getting the method. Returning word");
-////                    response += " " + word;
-////                }
-////            } else {
-////                response += " " + word;
-////            }
-////        }
-////        return response;
-////    }
-//
-//    public static void main(String args []) {
-//        Server server = new Server();
-//        server.run();
-//    }
-//
-//}
+package com.github.nez;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server implements Runnable {
+
+    private ServerSocket serverSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private Socket socket;
+    boolean listening = true;
+    private ArrayList<Client> connectedClients;
+
+    public Server() {
+        try {
+            this.serverSocket = new ServerSocket(4444);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                this.socket = this.serverSocket.accept();
+                System.out.println(in.readLine());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    public static void main(String args []) {
+        Server server = new Server();
+        server.run();
+    }
+
+}
