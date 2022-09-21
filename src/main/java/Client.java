04f0c82fc2460589c3b27fd37aecbdc7ec421d68
Montaniz.Stills <montanizstills@gmail.com>
2022-09-21 14:36:58 -0400

    Socket programming mvp;

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class Client {
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private Socket socket;
+    private DataOutputStream dataOutputStream;
+
+
+    public void close(){
+        try {
+            this.dataOutputStream.close();
+            this.socket.close();
+        } catch (IOException e) {
+            throw new Error(e);
+        }
+    }
+
+    public void write(String message){
+        try {
+            this.dataOutputStream = new DataOutputStream(this.socket.getOutputStream());
+            this.dataOutputStream.writeUTF(message);
+            this.dataOutputStream.flush();
+        } catch (IOException e) {
+            throw new Error(e);
+        }
+
+    }
+
+    public DataOutputStream getDataOutputStream() {
+        return dataOutputStream;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write a message here:\n");
+        String message = scanner.nextLine();
+        Client client = new Client();
+        try {
+            client.socket = new Socket("localhost",1234);
+            client.write(message);
+        } catch (IOException e) {
+            throw new Error(e);
+        }
+    }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,2 +1,109 @@
-package PACKAGE_NAME;public class Server {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private final Integer port;
+    private Socket socket;
+    private ServerSocket serverSocket;
+
+    private DataInputStream dataInputStream;
+
+
+    public Server(Integer port) {
+        this.port = port;
+    }
+
+
+    /**
+     * Will attempt to close the ServerSocket of this instance.
+     */
+    public void close() {
+        try {
+            this.serverSocket.close();
+        } catch (IOException e) {
+            System.out.println("Failed closing server socket..\n");
+            throw new Error(e);
+        }
+    }
+
+    /**
+     * Will attempt to establish a connection between the server and the client.
+     *
+     * @return a new socket representing the connection between the server and client.
+     */
+    private Socket accept(Integer port) {
+        try {
+            this.serverSocket = new ServerSocket(port);
+            return this.socket = this.serverSocket.accept();
+        } catch (IOException e) {
+            throw new Error(e);
+        }
+    }
+
+    /**
+     * Facade -- accept(int port)
+     *
+     * @return a new socket representing the connection between the server and client.
+     */
+    public Socket accept() {
+        return this.accept(this.port);
+    }
+
+    /**
+     * Will read the contents of the DataInputStream and convert into a String.
+     *
+     * @return The decoded String version of the DataInputStream's contents.
+     */
+    public String read() {
+        String message = null;
+        try {
+            this.dataInputStream = new DataInputStream(this.socket.getInputStream());
+            message = this.dataInputStream.readUTF();
+        } catch (IOException e) {
+            throw new Error(e);
+        }
+        this.close();
+        return message;
+    }
+
+    /**
+     * A socket is an endpoint for communications between machines.
+     * Will retrieve the Socket for this instance of a server.
+     *
+     * @return The instance's socket.
+     */
+    public Socket getSocket() {
+        return this.socket;
+    }
+
+    /**
+     * Will retrieve the ServerSocket for this instance.
+     *
+     * @return This instance's ServerSocket.
+     */
+    public ServerSocket getServerSocket() {
+        return this.serverSocket;
+    }
+
+    /**
+     * Will retrieve the DataInput stream for this instance.
+     *
+     * @return This instance's DataInputStream.
+     */
+    public DataInputStream getDataInputStream() {
+        return dataInputStream;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server(1234);
+        server.accept();
+        System.out.println(server.read());
+    }
+
 }
+
